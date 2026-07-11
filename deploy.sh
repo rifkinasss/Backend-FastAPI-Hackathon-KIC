0b539402a899103499ec11fdeba50a8f25d44211
rifkinasss <anashirulrifki@gmail.com>
2026-07-11 11:57:59 +0800

    chore: include database schema update in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ git pull
 echo "Activate virtual environment..."
 source .venv/bin/activate
 
+echo "Updating database schema..."
+PGPASSWORD="kic_user01" psql -h localhost -p 5432 -U kic_user -d iot_fuzzy_kideco -f database/schema.sql
+
 echo "Install/update dependencies..."
 pip install -r requirements.txt
 
